2e97d94bfd183f4177d160e22ab9e54e153ec17d
paulstefanday <paul@paulday.com.au>
2015-09-19 19:33:52 +1000

    test(Compile): compiled js into dist folder

---
+++ dist/index.js
@@ -0,0 +1,30 @@
+'use strict';
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
+
+var _uniqueRandomArray = require('unique-random-array');
+
+var _uniqueRandomArray2 = _interopRequireDefault(_uniqueRandomArray);
+
+var _starwarsNamesJson = require('./starwars-names.json');
+
+var _starwarsNamesJson2 = _interopRequireDefault(_starwarsNamesJson);
+
+var getRandomItem = (0, _uniqueRandomArray2['default'])(_starwarsNamesJson2['default']);
+
+module.exports = {
+  all: _starwarsNamesJson2['default'],
+  random: random
+};
+
+function random(number) {
+  if (number === undefined) {
+    return getRandomItem();
+  } else {
+    var randomItems = [];
+    for (var i = 0; i < number; i++) {
+      randomItems.push(getRandomItem());
+    }
+    return randomItems;
+  }
+}
